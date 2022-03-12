635c5eca965d2402696b2e7f6c1f0eceafa724cb
William Meyer <williamjmiii@gmail.com>
2022-03-12 10:41:15 -0500

    copied routes and controller to start building :wildcard function

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -3,4 +3,8 @@ class ParamsController < ApplicationController
     input_value = params[:caps]
     render json: {message: "#{input_value.upcase}"}
   end
+  def wild_caps
+    input_value = params[:caps]
+    render json: {message: "#{input_value.upcase}"}
+  end
 end
